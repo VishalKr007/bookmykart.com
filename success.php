89e8b6df840068c6f7e9d53d509cf749dfa63fc0
Vishal Kumar <vishukr785@gmail.com>
2018-03-31 20:36:24 +0530

    Update success.php

--- success.php
+++ success.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['email'])) {
 $user_id = $_SESSION['user_id'];
 $item_ids_string = $_GET['itemsid'];
 
-//We will change the status of the items purchased by the user to 'Confirmed'
+//change the status of the items purchased by the user to 'Confirmed'
 $query = "UPDATE users_items SET status='Confirmed' WHERE user_id=" . $user_id . " AND item_id IN (" . $item_ids_string . ") and status='Added to cart'";
 mysqli_query($con, $query) or die($mysqli_error($con));
 ?>
